9b01ac24960e62b0c1658144686e142dfa97cb2f
Vignesh B <vigneshbv02@gmail.com>
2018-02-22 22:08:54 +0530

    rv

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var http=require('http').Server(app);
 var io=require('socket.io')(http);
 
 var data=0;
-var temp=0,hum=0;
 
 function notify(req,res,next)
 {
@@ -25,7 +24,7 @@ io.on("connection",function(socket)
 
     socket.join("room01-ehealth");
 
-    socket.emit("notify",{'message':"Welcome to E-Health"});
+    socket.emit("notify",{'message':"Welcome to E-Health care"});
 
 });
 
@@ -35,16 +34,24 @@ app.get('/',function(req,res){
 });
 app.get('/high',function(req,res){
     console.log("high");
-    io.to("room01-ehealth").emit('notify',{'message':"Grocery level is HIGH"});
+    io.to("room01-ehealth").emit('notify',{'message':"Heartbeat is HIGH"});
     res.send("done");
 });
 
 app.get('/low',function(req,res){
     console.log("low");
-    io.to("room01-ehealth").emit('notify',{'message':"Grocery level is LOW"});
+    io.to("room01-ehealth").emit('notify',{'message':"Heartbeat is LOW"});
     res.send("done");
 });
 
+app.post("/datum",function(req,res){
+    var d={
+        result:[{
+            storage:data
+        }]
+    };
+    res.send(JSON.stringify(d));
+});
 
 
 var server=http.listen(process.env.PORT || 5000,function(){
